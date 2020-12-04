d0bbb25320f6b371e9c3a5c88eaa2cb27072976d
corey-power <CPower1248@gmail.com>
2020-12-04 15:17:27 -0500

    data-test-id=plant-card

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -3,11 +3,14 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-  constructor() {
-    super();
-    this.state = {
-      plants: []
-    }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     plants: []
+  //   }
+  // }
+  state = {
+    plants: []
   }
 
   // when the component mounts:
@@ -29,7 +32,7 @@ export default class PlantList extends Component {
     return (
       <main className="plant-list">
         {this.state?.plants?.map((plant) => (
-          <div className="plant-card" key={plant.id}>
+          <div className="plant-card" data-testid="plant-card" key={plant.id}>
             <img className="plant-image" src={plant.img} alt={plant.name} />
             <div className="plant-details">
               <h2 className="plant-name">{plant.name}</h2>
