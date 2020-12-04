58ea8ed282db412ebb3e320792e5116f9aa6a716
corey-power <CPower1248@gmail.com>
2020-12-04 12:27:43 -0500

    Added tests (Met MVP)

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,41 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event"
+
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />)
 
-test("form shows success message on submit with form details", () => {});
+    const header = screen.getByText(/checkout form/i)
+
+    expect(header).toBeInTheDocument()
+});
+
+test("form shows success message on submit with form details", async () => {
+    render(<CheckoutForm />)
+
+    const fnInput = screen.getByLabelText(/first name/i)
+    const lnInput = screen.getByLabelText(/last name/i)
+    const addressInput = screen.getByLabelText(/address/i)
+    const cityInput = screen.getByLabelText(/city/i)
+    const stateInput = screen.getByLabelText(/state/i)
+    const zipInput = screen.getByLabelText(/zip/i)
+
+    userEvent.type(fnInput, "Corey")
+    userEvent.type(lnInput, "Power")
+    userEvent.type(addressInput, "1 Street Rd")
+    userEvent.type(cityInput, "Somecity")
+    userEvent.type(stateInput, "Somestate")
+    userEvent.type(zipInput, "12345")
+
+    const button = screen.getByRole("button")
+
+    userEvent.click(button)
+
+    const output = await screen.findByText(/woo-hoo/i)
+
+    expect(output).toBeInTheDocument()
+});
